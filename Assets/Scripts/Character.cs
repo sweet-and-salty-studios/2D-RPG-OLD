7f0d814d78d60317891b1db5bc9f28a806bfaff6
Mathias Mustonen <mathias.mustonen@gmail.com>
2018-10-17 09:04:28 +0300

    Assets added
    Assets added + Tile map started + player animation....

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    protected float moveSpeed = 4f;
+    protected Vector2 moveDirection;
+    [SerializeField]
+    private Vector2 lastMoveDirection;
+    [SerializeField]
+    private bool isMoving;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    protected virtual void Update()
+    {
+        Movement(moveDirection);
+    }
+
+    private void Movement(Vector2 moveDirection)
+    {
+        isMoving = false;
+        isMoving = moveDirection != Vector2.zero ? true : false;
+        animator.SetBool("IsMoving", isMoving);
+
+        if (isMoving)
+        {
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+            animator.SetFloat("X", moveDirection.x);
+            animator.SetFloat("Y", moveDirection.y);
+        }
+        else
+        {
+            lastMoveDirection = moveDirection;
+            animator.SetFloat("Last X", lastMoveDirection.x);
+            animator.SetFloat("Last Y", lastMoveDirection.y);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Player : Character
+{
+	private const string HORIZONTAL_AXIS = "Horizontal";
+	private const string VERTICAL_AXIS = "Vertical";
+
+	[SerializeField]
+	private Vector2 directionInputs = Vector2.zero;
+
+	protected override void Update ()
+	{
+		moveDirection = GetInputDirection();
+
+		base.Update();
+	}
+
+	private Vector2 GetInputDirection()
+	{
+		directionInputs = new Vector2(Input.GetAxisRaw(HORIZONTAL_AXIS), Input.GetAxisRaw(VERTICAL_AXIS));
+		directionInputs.Normalize();
+		return directionInputs;
+	}
+}
